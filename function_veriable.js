ebb52584818d7a01801ea336465ab1da3832bef6
Npradheepan <44838133+Npradheepan@users.noreply.github.com>
2021-06-26 23:24:34 +0200

    Create function_veriable.js

---
+++ function_veriable.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+
+  
+function workBlanks(myNOun, myAdjective, myverb, myAdverb){
+    var result ="";
+    result += "The"+myNOun+"is"+myAdjective+myAdverb+myverb;
+
+    return result;
+}
+console.log(workBlanks("mydoc","Very","Claver","too"))
+
+
+
+
+    </script>
+</body>
+</html>
